0f676a26a2858a5074b2780c348934e49f697573
Adam Palmer <theadampalmer@gmail.com>
2020-08-06 04:00:29 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-    "SELECT Count(tardies) FROM students;"
+    "SELECT total(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
